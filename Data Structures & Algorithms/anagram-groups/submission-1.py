80a9e1cdb66a06a5f7f26ae797e0fd702bbec97e
Yuki Yoshida <yuki92496@gmail.com>
2026-05-19 17:22:46 +0900

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+        for s in strs:
+            #１文字ずつの配列を文字列に連結する
+            key = ''.join(sorted(s))
+            #未登録の場合、キー用の空リストを作る
+            if key not in groups:
+                groups[key] = []
+            #ソートしたキーのdictに元々の文字列をvalueとして追加
+            groups[key].append(s)
+        return list(groups.values())
