e9f23e17c01d63c9458fa45cb5dfc0c473c19e9b
Yuki Yoshida <yuki92496@gmail.com>
2026-05-19 17:57:19 +0900

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in seen:
+                #seenに存在するdiffと同じ値のindexを返す
+                return [nums.index(diff), i]
+            #ループした値のdictをseenに代入
+            seen[num] = i 
+
+# 7-3=4
+# 4 in [] - false
+# seen[3] = 0
+# {3:0}
+
+# 7-4=3
+# 3 in [3] - true
+# seen[4] = 1
+# return [0,1]
